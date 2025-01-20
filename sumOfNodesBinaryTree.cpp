cfb036ae3fd1769f887553091734c2323da1a5d1
Suraj <surajyadav052005@gmail.com>
2025-01-20 10:56:25 +0530

    Sum Of Nodes In a Binary tree

--- sumOfNodesBinaryTree.cpp
+++ sumOfNodesBinaryTree.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 using namespace std;
 
-struct Node
+class Node
 {
+public:
     int key;
     Node *left, *right;
-};
 
-Node* newNode(int key)
-{
-    Node* node = new Node;
-    node->key = key;
-    node->left = node->right = NULL;
-    return(node);
-}
+    Node(int x)
+    {
+        key = x;
+        left =NULL;
+        right = NULL;
+    }
+
+};
 
 int addBT(Node *root)
 {
@@ -28,14 +29,14 @@ int addBT(Node *root)
 
 int main()
 {
-    Node *root = newNode(1);
-    root->left = newNode(2);
-    root->right = newNode(3);
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
-    root->right->left = newNode(6);
-    root->right->right = newNode(7);
-    root->right->left->right = newNode(8);
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    root->right->left->right = new Node(8);
 
     int sum = addBT(root);
     cout << "Sum =" << sum << endl;
