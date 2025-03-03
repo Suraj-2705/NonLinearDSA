a73538ed3cdec427b3b81840cf73339c4777a709
Suraj-2705 <surajyadav052005@gmail.com>
2025-03-03 10:44:49 +0530

    In Order Traversal

--- inOrderTraversal.cpp
+++ inOrderTraversal.cpp
@@ -29,11 +29,19 @@ void inorderTraversal(Node *root)
 
 int main()
 {
-    Node*root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
+    Node*root = new Node(10);
+    root->left = new Node(17);
+    root->right = new Node(21);
+    root->left->left = new Node(5);
+    root->left->left->left = new Node(32);
+
+    root->left->right = new Node(36);
+    root->left->right->left = new Node(44);
+    root->left->right->right = new Node(61);
+    root->right->right = new Node(75);
+    root->right->right->left = new Node(81);
+    root->right->right->left->right = new Node(85);
+    
 
     inorderTraversal(root);
 
